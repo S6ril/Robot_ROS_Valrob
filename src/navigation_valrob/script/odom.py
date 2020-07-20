b37877716abe255dda06c892e61601830cc5b72f
S6ril <cyril.tessier@gmail.com>
2020-07-20 16:07:54 +0200

    début de l'odometrie vaec la souris

--- src/navigation_valrob/script/odom.py
+++ src/navigation_valrob/script/odom.py
@@ -3,26 +3,47 @@ import sys
 import usb.core
 import usb.util
 from math import atan2
+import time
 
-from geometry_msgs.msg import Pose2D
 
 
-def toNegative(value):
-    if value > 127:
-        value = value - 255
-    
-    return value
+class MouseData(object):
+    """Classe pour stocker les datas de la mouse
+    """
+    def __init__(self):
+        super(MouseData, self).__init__()
+        self.x = 0
+        self.y = 0
 
 class Odometer(object):
-    
+    """Classe pour mesurer une distance avec une souris d'ordinateur.
+    """
     def __init__(self, IDVendor, IDProduct):
+        """Pour obtenir les ID de la souris, sous linux :
+        - lsusb
+        à partir de là on identifie la souris, dans mon cas : "Bus 001 Device 016: ID 046d:c05a Logitech, Inc. M90/M100 Optical Mouse"
+        IDVendor : 0x+ 046d
+        IDProcduct : 0x+ c05a
+        
+        Puis avant de lancer le programme, on donne les droits de lecture et d'écriture sur la souris :
+        - sudo chmod a+rw /dev/bus/usb/001/016
+        Permet de capter les déplacements de la souris
+
+        :param IDVendor: identificateur unique de la souris
+        :type IDVendor: int
+        :param IDProduct: identificateur unique de la souris
+        :type IDProduct: int
+        """
         super(Odometer, self).__init__()
         self.dev = usb.core.find(idVendor=IDVendor, idProduct=IDProduct)
         self.interface = 0
         self.endpoint = self.dev[0][(0, 0)][0]
         self.data = 0
         
-        self.robotPose = Pose2D()
+        self.robotData = MouseData()
+        self.robotPose = MouseData()
+
+
 
         if self.dev.is_kernel_driver_active(self.interface) is True:
             # tell the kernel to detach
@@ -31,32 +52,39 @@ class Odometer(object):
             usb.util.claim_interface(self.dev, self.interface)
 
     
-    def read_speed(self):
+    def read_mouse(self):
         try:
             # Lorsque l'on dirige la souris vers le haut, Y de-croissant !!
             self.data = self.dev.read(self.endpoint.bEndpointAddress, self.endpoint.wMaxPacketSize)
-            self.robotPose.x = toNegative(self.data[1])
-            self.robotPose.y = toNegative(self.data[2])
-
-            self.robotPose.theta = atan2(self.robotPose.x, 15)
+            self.robotData.x = self.data[1]
+            self.robotData.y = self.data[2]
         
         except usb.core.USBError as e:
-            self.data = None
-            self.robotPose.x = 0
-            self.robotPose.y = 0
-            self.robotPose.theta = 0
+            # self.data = None
+            self.robotData.x = 0
+            self.robotData.y = 0
+
             if e.args == ('Operation timed out',):
                 print("ERROR")
 
 
 
+    def determine_position(self):
+        
+        self.read_mouse()
 
+        if self.robotData.x >= 129:
+            self.robotData.x = -(~self.robotData.x & 0xFF)
 
-    
-    def __del__(self):
-        usb.util.release_interface(self.dev, self.interface)
-        # reattach the device to the OS kernel
-        self.dev.attach_kernel_driver(self.interface)
+        if self.robotData.y >= 129:
+            self.robotData.y = -(~self.robotData.y & 0xFF)
+        
+        self.robotPose.x += self.robotData.x
+        self.robotPose.y -= self.robotData.y #Changement de signe pour un affichage matplotlib ensuite
+        
+
+        draw(self.robotPose.x, self.robotPose.y)
+
 
 
 
@@ -64,14 +92,30 @@ class Odometer(object):
     
 
 if __name__ == "__main__":
-    odometer = Odometer(0x046d, 0xc018)
+    import matplotlib.pyplot as plt
+
+    def draw(x, y):
+        plt.scatter(x, y)
+        plt.show()
+        plt.pause(0.0001)
+
+    odometer = Odometer(0x046d, 0xc05a)
+
+    plt.ion()
+    fig = plt.figure()
+
 
 
     while True:
         try:
-            odometer.read_speed()
-            print("speed X =",odometer.robotPose)
-        
+            #odometer.read_mouse()
+            #print("mouse =", odometer.robotData.__dict__)
+
+            # draw(odometer.robotSpeed.linear.x, odometer.robotSpeed.linear.y)
+
+            odometer.determine_position()
+            print(odometer.robotPose.__dict__)
+
         except KeyboardInterrupt:
             break
 
