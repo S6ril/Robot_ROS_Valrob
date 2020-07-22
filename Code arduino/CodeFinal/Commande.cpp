8b3a1710263ccbc9ec616bc287667e32b536b027
S6ril <cyril.tessier@gmail.com>
2020-07-22 20:37:24 +0200

    clean

--- Code arduino/CodeFinal/Commande.cpp
+++ Code arduino/CodeFinal/Commande.cpp
@@ -3,59 +3,67 @@
 
 
 
-void Commande::executerGcode(String commande){
-  gcode.DecompoGcode(commande); //Décomposition du Gcode dans les différentes variables
-  // gcode.affiche();
+void Commande::executerGcode(String commande)
+/**
+ * @brief Fonction qui permet d'exécuter les commandes demandées.
+ * 
+ */
+{
+    gcode.DecompoGcode(commande); //Décomposition du Gcode dans les différentes variables
+    // gcode.affiche();
 
-  // Test de la présence de le lettre M
-  // M en premier, car c'est elle qui determine l'arret des moteurs.
-  if (gcode.hasM()){
-    switch (gcode.m_M){
-      case 0:
-        // Arret
-        break;
-
-      case 1:
-        // Commande à définir
-        break;
-
-      case 666:
-        if (Serial.available() )
+    // Test de la présence de le lettre M
+    // M en premier, car c'est elle qui determine l'arret des moteurs.
+    if (gcode.hasM())
+    {
+        switch (gcode.m_M)
         {
-          Serial.println("Com OK");   
+        case 0:
+            // Commande à définir
+            break;
+
+        case 1:
+            // Commande à définir
+            break;
+
+        case 666:
+            if (Serial.available() )
+            {
+                Serial.println("Com OK");   
+            }
+            break;
         }
-        break;
     }
-  }
 
-  // Test de la présence de G
-  else if (gcode.hasG()){
-    //Test de la varible G
-    switch (gcode.m_G){
-      case 20:
-      // Commande
-      break;
+    // Test de la présence de G
+    else if (gcode.hasG())
+    {
+        //Test de la varible G
+        switch (gcode.m_G)
+        {
+        case 20:
+            // Commande
+            break;
 
-      case 1:
-      // Commande à définir
-      break;
+        case 1:
+            // Commande à définir
+            break;
+        }
     }
-  }
 
-  // Variable de Test en L
-  else if (gcode.hasL())
-  {
-    switch (int(gcode.m_L)){
-      case 0:
-      digitalWrite(3, LOW);
-      break;
+    // Variable de Test en L
+    else if (gcode.hasL())
+    {
+        switch (int(gcode.m_L))
+        {
+        case 0:
+            digitalWrite(3, LOW);
+            break;
 
-      case 1:
-      digitalWrite(3, HIGH);
-      break;
+        case 1:
+            digitalWrite(3, HIGH);
+            break;
+        }
     }
-  }
-
-
 
 }
