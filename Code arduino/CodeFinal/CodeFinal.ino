944a85b3ed27f6c5cdaa72cc4e9fc371121dff8f
S6ril <cyril.tessier@gmail.com>
2020-07-22 20:33:21 +0200

    clean

--- Code arduino/CodeFinal/CodeFinal.ino
+++ Code arduino/CodeFinal/CodeFinal.ino
@@ -2,39 +2,32 @@
 #include "Commande.h"
 
 
-String message;
-
-
 Commande commande;
 //Creation de l'objet pour traduire le Gcode
 
 
 
 void setup() {
-  Serial.begin(9600); // open the serial port at 9600 bps:
-  Serial.flush();
+    Serial.begin(9600); // open the serial port at 9600 bps:
+    Serial.flush();
 
-  //Test Led
-  pinMode(3, OUTPUT);
+    //Test Led
+    pinMode(3, OUTPUT);
 }
 
-void loop() {
+void loop() 
+{
+    int i=0;
+    char commandebuffer[200] = {'\0'};
 
-
-  int i=0;
-  char commandebuffer[200] = {'\0'};
-
-  if (Serial.available())  {
-      delay(100);
-      while (Serial.available() && i<99){
-          commandebuffer[i++] = Serial.read();
-      }
+    if (Serial.available())
+    {
+        delay(100);
+        while (Serial.available() && i<200)
+        {
+            commandebuffer[i++] = Serial.read();
+        }
     commandebuffer[i++]='\0';
     }
-    //Serial.print("Message =");
-    //Serial.println(message);
     commande.executerGcode(commandebuffer);
-
-
-
 }
